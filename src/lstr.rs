79b39d89e837f98671a9cdc129a796349b611a03
Paulo Carabalone <psoreto@outlook.com>
2025-12-04 08:39:30 -0300

    optimize LStr with static dispatch and pointer-based hashing

--- src/lstr.rs
+++ src/lstr.rs
@@ -1,108 +1,207 @@
-use std::{marker::PhantomData, rc::Rc};
+use std::{
+    hash::{Hash, Hasher},
+    rc::Rc,
+};
 
 use crate::{
     Borrowed, Mode, Owned, from_lua::FromLua, is_type::IsType, lua::InnerLua, sys, to_lua::ToLua,
 };
 
-pub type StackStr = LStr<Borrowed>;
+pub trait StringState {
+    type State;
+}
 
-pub type StrRef = LStr<Owned>;
+pub trait StringAccess {
+    fn as_slice<'a>(&'a self) -> &'a [u8];
 
-#[derive(Debug)]
-pub struct OwnedLStr<M: Mode>(Rc<InnerLua>, i32, PhantomData<M>);
+    fn as_str<'a>(&'a self) -> Option<&'a str> {
+        str::from_utf8(self.as_slice()).ok()
+    }
+}
 
-impl<M> Drop for OwnedLStr<M>
-where
-    M: Mode,
-{
-    fn drop(&mut self) {
-        if let Some(ptr) = self.0.try_state() {
-            unsafe { sys::luaL_unref(ptr, sys::LUA_REGISTRYINDEX, self.1) };
-        }
+pub struct BorrowedState {
+    ptr: *mut sys::lua_State,
+    idx: i32,
+    slice: &'static [u8],
+}
+
+impl StringState for Borrowed {
+    type State = BorrowedState;
+}
+
+impl StringAccess for BorrowedState {
+    fn as_slice<'a>(&'a self) -> &'a [u8] {
+        self.slice
     }
 }
 
 #[derive(Debug)]
-pub enum LStr<M: Mode> {
-    Borrowed(*mut sys::lua_State, i32, &'static [u8]),
-    Owned(Rc<OwnedLStr<M>>, &'static [u8]),
+pub struct OwnedInner {
+    lua: Rc<InnerLua>,
+    id: i32,
+}
+
+impl Drop for OwnedInner {
+    fn drop(&mut self) {
+        if let Some(ptr) = self.lua.try_state() {
+            unsafe { sys::luaL_unref(ptr, sys::LUA_REGISTRYINDEX, self.id) };
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct OwnedState {
+    inner: Rc<OwnedInner>,
+    slice: &'static [u8],
+}
+
+impl StringState for Owned {
+    type State = OwnedState;
+}
+
+impl StringAccess for OwnedState {
+    fn as_slice<'a>(&'a self) -> &'a [u8] {
+        let _ = self.inner.lua.state();
+        self.slice
+    }
+}
+
+pub type StackStr = LStr<Borrowed>;
+pub type StrRef = LStr<Owned>;
+
+pub struct LStr<M>
+where
+    M: Mode + StringState,
+    M::State: StringAccess,
+{
+    state: M::State,
 }
 
 impl<M> LStr<M>
 where
-    M: Mode,
+    M: Mode + StringState,
+    M::State: StringAccess,
 {
-    fn slice_from_stack(ptr: *mut sys::lua_State, idx: i32) -> &'static [u8] {
-        let mut len: usize = 0;
-        let str_ptr = unsafe { sys::lua_tolstring(ptr, idx, &mut len) };
-        let slice = unsafe { std::slice::from_raw_parts(str_ptr as *const u8, len) };
-        slice
-    }
-
-    pub(crate) fn new(inner: Rc<InnerLua>, value: &str) -> Self {
-        let ptr = inner.state();
-        value.to_lua(ptr);
-        let slice = Self::slice_from_stack(ptr, -1);
-        let id = unsafe { sys::luaL_ref(ptr, sys::LUA_REGISTRYINDEX) };
-        let inner = Rc::new(OwnedLStr(inner, id, PhantomData));
-        Self::Owned(inner, slice)
-    }
-
-    pub(crate) fn borrowed(ptr: *mut sys::lua_State, idx: i32) -> Self {
-        let idx = unsafe { sys::lua_absindex(ptr, idx) };
-        let slice = Self::slice_from_stack(ptr, idx);
-        Self::Borrowed(ptr, idx, slice)
-    }
-
-    pub(crate) fn owned(inner: Rc<InnerLua>, idx: i32, slice: &'static [u8]) -> Self {
-        unsafe {
-            let ptr = inner.state();
-            sys::lua_pushvalue(ptr, idx);
-            let id = sys::luaL_ref(ptr, sys::LUA_REGISTRYINDEX);
-            let owned_str = Rc::new(OwnedLStr(inner, id, PhantomData));
-            Self::Owned(owned_str, slice)
-        }
-    }
-
-    pub fn to_owned(&self) -> Self {
-        match self {
-            LStr::Borrowed(ptr, idx, slice) => Self::owned(InnerLua::from_ptr(*ptr), *idx, *slice),
-            LStr::Owned(inner, slice) => Self::Owned(inner.clone(), slice),
-        }
-    }
-
     pub fn as_slice<'a>(&'a self) -> &'a [u8] {
-        match self {
-            LStr::Borrowed(_, _, slice) => slice,
-            LStr::Owned(inner, slice) => {
-                let _ = inner.0.state();
-                slice
-            }
-        }
+        self.state.as_slice()
     }
 
-    pub fn as_str(&self) -> Option<&str> {
-        str::from_utf8(self.as_slice()).ok()
-    }
-
-    pub fn to_vec(&self) -> Vec<u8> {
-        self.as_slice().to_vec()
+    pub fn as_str<'a>(&'a self) -> Option<&'a str> {
+        self.state.as_str()
     }
 }
 
-impl<M> Clone for LStr<M>
-where
-    M: Mode,
-{
+impl StackStr {
+    pub fn from_stack(ptr: *mut sys::lua_State, idx: i32) -> StackStr {
+        let idx = unsafe { sys::lua_absindex(ptr, idx) };
+        let slice = slice_from_stack(ptr, idx);
+        Self {
+            state: BorrowedState { ptr, idx, slice },
+        }
+    }
+
+    pub fn to_owned(&self) -> StrRef {
+        StrRef::from_stack(self.state.ptr, self.state.idx)
+    }
+}
+
+impl StrRef {
+    pub fn new(lua: Rc<InnerLua>, value: &str) -> StrRef {
+        let ptr = lua.state();
+        value.to_lua(ptr);
+        let slice = slice_from_stack(ptr, -1);
+        let id = unsafe { sys::luaL_ref(ptr, sys::LUA_REGISTRYINDEX) };
+        let inner = Rc::new(OwnedInner { lua, id });
+        Self {
+            state: OwnedState { inner, slice },
+        }
+    }
+
+    pub fn from_stack(ptr: *mut sys::lua_State, idx: i32) -> StrRef {
+        unsafe {
+            let lua = InnerLua::from_ptr(ptr);
+            sys::lua_pushvalue(ptr, idx);
+            let slice = slice_from_stack(ptr, -1);
+            let id = sys::luaL_ref(ptr, sys::LUA_REGISTRYINDEX);
+
+            Self {
+                state: OwnedState {
+                    inner: Rc::new(OwnedInner { lua, id }),
+                    slice,
+                },
+            }
+        }
+    }
+}
+
+impl Clone for StrRef {
     fn clone(&self) -> Self {
-        self.to_owned()
+        Self {
+            state: self.state.clone(),
+        }
+    }
+}
+
+impl<M> std::fmt::Display for LStr<M>
+where
+    M: Mode + StringState,
+    M::State: StringAccess,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = String::from_utf8_lossy(self.as_slice());
+        write!(f, "{}", s)
+    }
+}
+
+impl<M> std::fmt::Debug for LStr<M>
+where
+    M: Mode + StringState,
+    M::State: StringAccess,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let slice = self.as_slice();
+        f.debug_struct("LStr")
+            .field("ptr", &slice.as_ptr())
+            .field("len", &slice.len())
+            .field("preview", &String::from_utf8_lossy(slice))
+            .finish()
+    }
+}
+
+impl<M> AsRef<[u8]> for LStr<M>
+where
+    M: Mode + StringState,
+    M::State: StringAccess,
+{
+    fn as_ref(&self) -> &[u8] {
+        self.as_slice()
+    }
+}
+
+impl<M> From<&LStr<M>> for Vec<u8>
+where
+    M: Mode + StringState,
+    M::State: StringAccess,
+{
+    fn from(value: &LStr<M>) -> Self {
+        value.state.as_slice().to_vec()
+    }
+}
+
+impl<M> From<LStr<M>> for Vec<u8>
+where
+    M: Mode + StringState,
+    M::State: StringAccess,
+{
+    fn from(value: LStr<M>) -> Self {
+        value.state.as_slice().to_vec()
     }
 }
 
 unsafe impl FromLua for StackStr {
-    fn from_lua(ptr: *mut mlua_sys::lua_State, idx: i32) -> Option<Self> {
+    fn from_lua(ptr: *mut mlua_sys::lua_State, idx: i32) -> Option<StackStr> {
         if unsafe { sys::lua_type(ptr, idx) } == sys::LUA_TSTRING as i32 {
-            Some(Self::borrowed(ptr, idx))
+            Some(StackStr::from_stack(ptr, idx))
         } else {
             None
         }
@@ -112,49 +211,79 @@ unsafe impl FromLua for StackStr {
 unsafe impl FromLua for StrRef {
     fn from_lua(ptr: *mut mlua_sys::lua_State, idx: i32) -> Option<Self> {
         if unsafe { sys::lua_type(ptr, idx) } == sys::LUA_TSTRING as i32 {
-            let slice = Self::slice_from_stack(ptr, idx);
-            Some(Self::owned(InnerLua::from_ptr(ptr), idx, slice))
+            Some(StrRef::from_stack(ptr, idx))
         } else {
             None
         }
     }
 }
 
-unsafe impl<M> ToLua for &LStr<M>
-where
-    M: Mode,
-{
+unsafe impl ToLua for &StackStr {
     fn to_lua(self, ptr: *mut mlua_sys::lua_State) {
-        match self {
-            LStr::Borrowed(_, idx, _) => unsafe {
-                sys::lua_pushvalue(ptr, *idx);
-            },
-            LStr::Owned(inner, _) => unsafe {
-                sys::lua_rawgeti(ptr, sys::LUA_REGISTRYINDEX, inner.1 as _);
-            },
-        }
+        unsafe { sys::lua_pushvalue(ptr, self.state.idx) };
     }
 }
 
-unsafe impl<M> ToLua for LStr<M>
-where
-    M: Mode,
-{
+unsafe impl ToLua for &StrRef {
     fn to_lua(self, ptr: *mut mlua_sys::lua_State) {
-        unsafe {
-            match self {
-                LStr::Borrowed(_, idx, _) => sys::lua_pushvalue(ptr, idx),
-                LStr::Owned(inner, _) => inner.0.push_ref(ptr, inner.1),
-            }
-        }
+        unsafe { sys::lua_rawgeti(ptr, sys::LUA_REGISTRYINDEX, self.state.inner.id as _) };
+    }
+}
+
+unsafe impl ToLua for StackStr {
+    fn to_lua(self, ptr: *mut mlua_sys::lua_State) {
+        (&self).to_lua(ptr);
+    }
+}
+
+unsafe impl ToLua for StrRef {
+    fn to_lua(self, ptr: *mut mlua_sys::lua_State) {
+        (&self).to_lua(ptr);
     }
 }
 
 impl<M> IsType for LStr<M>
 where
-    M: Mode,
+    M: Mode + StringState,
+    M::State: StringAccess,
 {
     fn is_type(ptr: *mut crate::sys::lua_State, idx: i32) -> bool {
         (unsafe { sys::lua_type(ptr, idx) } == sys::LUA_TSTRING as i32)
     }
+}
+
+impl<M1: Mode, M2: Mode> PartialEq<LStr<M2>> for LStr<M1>
+where
+    M1: Mode + StringState,
+    M1::State: StringAccess,
+    M2: Mode + StringState,
+    M2::State: StringAccess,
+{
+    fn eq(&self, other: &LStr<M2>) -> bool {
+        self.state.as_slice().as_ptr() == other.state.as_slice().as_ptr()
+    }
+}
+
+impl<M: Mode> Eq for LStr<M>
+where
+    M: Mode + StringState,
+    M::State: StringAccess,
+{
+}
+
+impl<M: Mode> Hash for LStr<M>
+where
+    M: Mode + StringState,
+    M::State: StringAccess,
+{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.state.as_slice().as_ptr().hash(state);
+    }
+}
+
+fn slice_from_stack(ptr: *mut sys::lua_State, idx: i32) -> &'static [u8] {
+    let mut len: usize = 0;
+    let str_ptr = unsafe { sys::lua_tolstring(ptr, idx, &mut len) };
+    let slice = unsafe { std::slice::from_raw_parts(str_ptr as *const u8, len) };
+    slice
 }
